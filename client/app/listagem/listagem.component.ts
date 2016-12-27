d9d9476c19051593179c35dd6138dc50ca41d650
edisonfm <edisonferraz@gmail.com>
2016-12-27 15:35:34 -0200

    Implementando metodo remover fotos

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -10,13 +10,38 @@ import { FotoComponent } from './../foto/foto.component';
 export class ListagemComponent {
 
     fotos: FotoComponent[] = [];
+    service: FotoService;
+    mensagem: string = '';
 
     constructor(service: FotoService) {
 
-        service
+        this.service = service;
+
+        this.service
             .lista()
             .subscribe(fotos => {
                 this.fotos = fotos;
             }, erro => console.log(erro))
+    }
+
+    remove(foto) {
+
+        this.service
+            .remove(foto)
+            .subscribe(
+                () => {
+
+                    console.log('Foto removida com sucesso');
+                    let novasFotos = this.fotos.slice(0);
+                    let indice = novasFotos.indexOf(foto);
+                    novasFotos.splice(indice, 1);
+                    this.fotos = novasFotos;
+                    this.mensagem = 'Foto removida com sucesso';
+                },
+                erro =>  {
+                    console.log(erro);
+                    this.mensagem = 'Não foi possivel remover a foto';
+                }
+            );
     }
 }
